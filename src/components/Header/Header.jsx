672f519ad1a72030037048392c3945e77e31419a
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-02-25 14:45:28 +0200

    feat: add Header markup and import Navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom';
+import Navigation from '../Navigation/Navigation';
+import css from './Header.module.css';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={css.header}>
+      <div className={css.container}>
+        <Link href="/" className={css.logo}>
+          <svg className={css.icon}>
+            <use href="/src/assets/icons/symbol-defs.svg#icon-logo"></use>
+          </svg>
+        </Link>
+        <Navigation />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
