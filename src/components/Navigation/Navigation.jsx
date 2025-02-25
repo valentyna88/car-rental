2c20084db4d78d1f568d01e5cfcda2b01facf35f
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-02-25 13:25:26 +0200

    feat: add Navigation markup

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,22 @@
+import { NavLink } from 'react-router-dom';
+import css from './Navigation.module.css';
+import clsx from 'clsx';
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
+
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <nav className={css.nav}>
+      <NavLink to="/" className={buildLinkClass}>
+        Home
+      </NavLink>
+      <NavLink to="/catalog" className={buildLinkClass}>
+        Catalog
+      </NavLink>
+    </nav>
+  );
 };
 
 export default Navigation;
