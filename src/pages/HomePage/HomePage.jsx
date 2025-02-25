84d0135db892cffda3b580cbfb812754931fb878
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-02-25 16:01:46 +0200

    feat(homepage): add HomePage markup

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom';
+import css from './HomePage.module.css';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <section className={css.hero}>
+      <div className={css.container}>
+        <h1 className={css.title}>Find your perfect rental car</h1>
+        <p className={css.text}>
+          Reliable and budget-friendly rentals for any journey
+        </p>
+        <Link to="/catalog" className={css.button}>
+          View Catalog
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default HomePage;
