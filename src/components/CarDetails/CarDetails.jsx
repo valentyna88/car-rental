414632598441b10fbe7cdd0ffca33f7fdf0f8521
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-02-27 13:25:32 +0200

    feat: add styles for CardDetails and update CardDetails markup

--- src/components/CarDetails/CarDetails.jsx
+++ src/components/CarDetails/CarDetails.jsx
@@ -1,4 +1,5 @@
 import { formatMileage, getShortAddress, shortenId } from '../../utils/utils';
+import sprite from '../../assets/icons/sprite.svg';
 import css from './CarDetails.module.css';
 
 const CarDetails = ({ car }) => {
@@ -23,40 +24,88 @@ const CarDetails = ({ car }) => {
   const allFeatures = [...accessories, ...functionalities];
 
   return (
-    <section>
+    <section className={css.carDetails}>
       <img src={img} alt={model} className={css.carImage} />
       <div className={css.details}>
-        <h2>
-          {brand} {model}, {year}
-        </h2>
-        <p>Id: {shortenId(id)}</p>
-      </div>
-      <div className={css.details}>
-        <p className={css.location}>
-          {getShortAddress(address)} Mileage: {formatMileage(mileage)} km
-        </p>
-        <p className={css.price}>${rentalPrice}</p>
-        <p>{description}</p>
+        <div className={css.mainDetails}>
+          <div className={css.titleContainer}>
+            <h2 className={css.title}>
+              {brand} {model}, {year}
+            </h2>
+            <p className={css.id}>Id: {shortenId(id)}</p>
+          </div>
+          <div className={css.locationMileageContainer}>
+            <div className={css.locationContainer}>
+              <svg className={css.icon}>
+                <use xlinkHref={`${sprite}#icon-location`}></use>
+              </svg>
+              <span>{getShortAddress(address)}</span>
+            </div>
+            <p>Mileage: {formatMileage(mileage)} km</p>
+          </div>
+          <p className={css.price}>${rentalPrice}</p>
+          <p className={css.description}>{description}</p>
+        </div>
 
-        <h3>Rental Conditions:</h3>
-        <ul>
-          {rentalConditions.map((condition, index) => (
-            <li key={index}>{condition}</li>
-          ))}
-        </ul>
+        <div className={css.detailsSection}>
+          <div className={css.conditions}>
+            <h3 className={css.listTitle}>Rental Conditions:</h3>
+            <ul>
+              {rentalConditions.map((condition, index) => (
+                <li className={css.detailsItem} key={index}>
+                  <svg className={css.icon}>
+                    <use xlinkHref={`${sprite}#icon-check-circle`}></use>
+                  </svg>
+                  {condition}
+                </li>
+              ))}
+            </ul>
+          </div>
 
-        <h3>Car Specifications:</h3>
-        <p>Year: {year}</p>
-        <p>Type: {type}</p>
-        <p>Fuel Consumption: {fuelConsumption}</p>
-        <p>Engine Size: {engineSize}</p>
+          <div className={css.specifications}>
+            <h3 className={css.listTitle}>Car Specifications:</h3>
+            <ul>
+              <li className={css.detailsItem}>
+                <svg className={css.icon}>
+                  <use xlinkHref={`${sprite}#icon-calendar`}></use>
+                </svg>
+                Year: {year}
+              </li>
+              <li className={css.detailsItem}>
+                <svg className={css.icon}>
+                  <use xlinkHref={`${sprite}#icon-car`}></use>
+                </svg>
+                Type: {type}
+              </li>
+              <li className={css.detailsItem}>
+                <svg className={css.icon}>
+                  <use xlinkHref={`${sprite}#icon-fuel-pump`}></use>
+                </svg>
+                Fuel Consumption: {fuelConsumption}
+              </li>
+              <li className={css.detailsItem}>
+                <svg className={css.icon}>
+                  <use xlinkHref={`${sprite}#icon-gear`}></use>
+                </svg>
+                Engine Size: {engineSize}
+              </li>
+            </ul>
+          </div>
 
-        <h3>Accessories and Functionalities:</h3>
-        <ul>
-          {allFeatures.map((feature, index) => (
-            <li key={`feature-${index}`}>{feature}</li>
-          ))}
-        </ul>
+          <div className={css.features}>
+            <h3 className={css.listTitle}>Accessories and Functionalities:</h3>
+            <ul>
+              {allFeatures.map((feature, index) => (
+                <li className={css.detailsItem} key={`feature-${index}`}>
+                  <svg className={css.icon}>
+                    <use xlinkHref={`${sprite}#icon-check-circle`}></use>
+                  </svg>
+                  {feature}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </section>
   );
