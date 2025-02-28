1e4d14c748018502484e4f36165ab473ab67d712
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-02-28 12:59:43 +0200

    feat(store): add redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,36 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import { carsReducer } from './cars/slice';
 
-const store = configureStore({
+const persistConfig = {
+  key: 'favorites',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig);
+
+export const store = configureStore({
   reducer: {
     cars: carsReducer,
+    favorites: persistedReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export default store;
+export const persistor = persistStore(store);
